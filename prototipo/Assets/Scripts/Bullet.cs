dfe22745a91da1bf9aa9024e03d02ffac78caa6b
sebdeveloper6952 <sebdeveloper6952@gmail.com>
2019-05-14 09:08:25 -0600

    disparo mejorado.

---
+++ prototipo/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletSpeed = 1.0f;
+    public float bulletLifetime = 5.0f;
+
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _rb.velocity = transform.TransformDirection(Vector3.forward) * bulletSpeed;
+        Destroy(gameObject, bulletLifetime);
+    }
+}
---
+++ prototipo/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float shootingCooldown = 0.5f;
+    public Transform bulletPoint;
+    public GameObject bulletPrefab;
+    public Camera mainCamera;
+
+    private float _curCooldown;
+    private bool _shot;
+
+    private void Start()
+    {
+        _curCooldown = shootingCooldown;
+    }
+
+    private void Update()
+    {
+        // Debug bullet direction
+        Debug.DrawRay(bulletPoint.position, transform.forward * 5.0f, Color.red);
+        
+        if (Input.GetMouseButton(0) && !_shot)
+        {
+            _curCooldown = shootingCooldown;
+            _shot = true;
+            ShootBullet();
+        }
+
+        if (_shot)
+        {
+            _curCooldown -= Time.deltaTime;
+        }
+
+        if (_curCooldown < 0f)
+        {
+            _curCooldown = shootingCooldown;
+            _shot = false;
+        }
+        
+        Debug.Log(string.Concat("Camera: ", mainCamera.transform.rotation.eulerAngles));
+    }
+
+    private void ShootBullet()
+    {
+        Vector3 r = transform.rotation.eulerAngles;
+        Quaternion rot = Quaternion.Euler(mainCamera.transform.rotation.eulerAngles.x * 2f, r.y, r.z);
+        GameObject go = Instantiate(bulletPrefab, bulletPoint.position, rot);
+    }
+}
